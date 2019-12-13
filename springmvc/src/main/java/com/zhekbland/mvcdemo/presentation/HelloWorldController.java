d89a93b3973c2f2d568f4f4674df46b82970d346
zhekbland <eshpytev@mail.ru>
2019-12-13 21:52:48 +0300

    Spring MVC. Using @RequestParam.

--- springmvc/src/main/java/com/zhekbland/mvcdemo/presentation/HelloWorldController.java
+++ springmvc/src/main/java/com/zhekbland/mvcdemo/presentation/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.zhekbland.mvcdemo.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -37,6 +38,28 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    /**
+     * Get request param and put it into theName via @RequestParam
+     *
+     * @param theName get request param
+     * @param model for response
+     * @return jsp view
+     */
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my Friend from v3! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);

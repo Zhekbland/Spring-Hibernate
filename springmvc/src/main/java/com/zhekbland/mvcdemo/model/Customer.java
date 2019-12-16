2c18e86bb93a33e4ccd1fa54016429cc02febcef
zhekbland <eshpytev@mail.ru>
2019-12-16 16:55:50 +0300

    Spring MVC. Form validation. Regexp for validation.

--- springmvc/src/main/java/com/zhekbland/mvcdemo/model/Customer.java
+++ springmvc/src/main/java/com/zhekbland/mvcdemo/model/Customer.java
@@ -1,9 +1,6 @@
 package com.zhekbland.mvcdemo.model;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
 
@@ -25,6 +22,14 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
+
+    /**
+     * Adding validation for require fields and
+     * regexp for the value
+     * And catching errors
+     */
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -48,5 +53,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
--- springmvc/src/main/webapp/WEB-INF/view/customer-form.jsp
+++ springmvc/src/main/webapp/WEB-INF/view/customer-form.jsp
@@ -18,6 +18,8 @@
         Free passes: <form:input path="freePasses"/>
         <form:errors path="freePasses" cssClass="error"/>
         <br><br>
+        Postal code: <form:input path="postalCode"/>
+        <form:errors path="postalCode" cssClass="error"/>
         <input type="submit" value="Submit"/>
     </form:form>
 

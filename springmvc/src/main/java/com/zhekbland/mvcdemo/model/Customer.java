f7f7d6fc85b0331832ae9afc2ec5fd8126735a04
zhekbland <eshpytev@mail.ru>
2019-12-16 13:04:57 +0300

    Spring MVC. Form validation. Boundaries of a value.

--- springmvc/src/main/java/com/zhekbland/mvcdemo/model/Customer.java
+++ springmvc/src/main/java/com/zhekbland/mvcdemo/model/Customer.java
@@ -1,5 +1,7 @@
 package com.zhekbland.mvcdemo.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -15,6 +17,15 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    /**
+     * Adding validation for require fields and
+     * define the boundaries of the value
+     * And catching errors
+     */
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
+
     public String getFirstName() {
         return firstName;
     }
@@ -29,5 +40,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }

52f2a9d01519134c09e90cd4e2918b0c68bc1802
zhekbland <eshpytev@mail.ru>
2020-02-09 14:36:49 +0300

    Spring AOP. @Pointcut. Reusing pointcut expression.

--- spring-AOP/src/main/java/com/zhekbland/aop/aspect/MyLoggingAspect.java
+++ spring-AOP/src/main/java/com/zhekbland/aop/aspect/MyLoggingAspect.java
@@ -2,13 +2,25 @@ package com.zhekbland.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyLoggingAspect {
-    @Before("execution(* add*(..))")
+
+    @Pointcut("execution(* add*(..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("Executing @Before advice");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("Performing API analytics");
     }
 }

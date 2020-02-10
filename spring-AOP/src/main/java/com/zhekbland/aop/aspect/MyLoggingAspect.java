9308bcdfbdfa0a3a49a68d16f178d6f19caf735f
zhekbland <eshpytev@mail.ru>
2020-02-10 18:23:19 +0300

    Spring AOP. JoinPoints. Display method signature.

--- spring-AOP/src/main/java/com/zhekbland/aop/aspect/MyLoggingAspect.java
+++ spring-AOP/src/main/java/com/zhekbland/aop/aspect/MyLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.zhekbland.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,10 @@ import org.springframework.stereotype.Component;
 public class MyLoggingAspect {
 
     @Before("com.zhekbland.aop.aspect.AopExpressions.forDaoNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("Executing @Before advice");
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
     }
 
 }

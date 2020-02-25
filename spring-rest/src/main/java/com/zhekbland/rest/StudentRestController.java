8083b2c7b54f1c3c7449346e5af85b2cef157d8d
zhekbland <eshpytev@mail.ru>
2020-02-25 13:05:49 +0300

    Spring-REST. Exception handling.

--- spring-rest/src/main/java/com/zhekbland/rest/StudentRestController.java
+++ spring-rest/src/main/java/com/zhekbland/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.zhekbland.rest;
 
 import com.zhekbland.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -36,6 +35,20 @@ public class StudentRestController {
 
     @GetMapping("/student/{studentId}")
     public Student getStudents(@PathVariable int studentId) {
+
+        if ((studentId >= this.students.size()) || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return this.students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }

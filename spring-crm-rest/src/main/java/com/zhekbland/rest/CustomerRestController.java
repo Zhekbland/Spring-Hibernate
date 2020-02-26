18baebd7d0156e868334bdb3074175aecf911b7d
zhekbland <eshpytev@mail.ru>
2020-02-26 15:23:31 +0300

    Spring-REST. CRUD Database Real-Time Project - Add Customer.

--- spring-crm-rest/src/main/java/com/zhekbland/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/zhekbland/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.zhekbland.rest;
 import com.zhekbland.entity.Customer;
 import com.zhekbland.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,13 @@ public class CustomerRestController {
         if (customer == null) {
             throw new CustomerNotFoundException();
         }
+        return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        this.customerService.saveCustomer(customer);
         return customer;
     }
 }

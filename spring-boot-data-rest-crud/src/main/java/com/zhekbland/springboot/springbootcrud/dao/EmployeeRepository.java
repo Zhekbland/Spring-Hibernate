6ccc1232174e5b73ba51cafd280c70af6edbb1a5
zhekbland <eshpytev@mail.ru>
2020-03-06 15:28:40 +0300

    Spring-Boot. Spring Data REST. Configuration Pagination Sorting.

--- spring-boot-data-rest-crud/src/main/java/com/zhekbland/springboot/springbootcrud/dao/EmployeeRepository.java
+++ spring-boot-data-rest-crud/src/main/java/com/zhekbland/springboot/springbootcrud/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.zhekbland.springboot.springbootcrud.dao;
 
 import com.zhekbland.springboot.springbootcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
